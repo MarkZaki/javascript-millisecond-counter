5916218fe33cf449eb2665d918918b4424c769e9
Mark Zaky <markzaky18@gmail.com>
2020-07-16 09:44:30 +0200

    refactor

--- counter.js
+++ counter.js
@@ -5,13 +5,8 @@ setInterval(function () {
   let seconds = Math.floor(milliSecondsTimer / 1000);
   seconds -= minutes * 60;
   let milliSeconds = milliSecondsTimer;
-  if (minutes === 0) {
-    milliSeconds -= Math.floor(seconds * 1000);
-  } else {
-    milliSeconds = milliSecondsTimer;
-    milliSeconds -= Math.floor(minutes * 60 * 1000);
-    milliSeconds -= Math.floor(seconds * 1000);
-  }
+  milliSeconds -= Math.floor(minutes * 60 * 1000);
+  milliSeconds -= Math.floor(seconds * 1000);
   console.log(`${minutes}:${seconds}:${milliSeconds}`);
   milliSecondsTimer++;
 }, 1);
